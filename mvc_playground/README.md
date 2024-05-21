60314fa579db185439cf13dcc552d6dcd566503e
minikuma <minikuma@naver.com>
2024-05-21 22:55:28 +0900

    init commit

---
+++ mvc_playground/README.md
@@ -0,0 +1,5 @@
+### To-Do
+- [ ] Web Server 만들기 (Tomcat)
+  - [ ] Dependency 추가
+- [ ] Front Controller
+- [ ] MVC 구현
---
+++ mvc_playground/src/main/java/me/minikuma/WebServerApp.java
@@ -0,0 +1,7 @@
+package me.minikuma;
+
+public class WebServerApp {
+    public static void main(String[] args) {
+        System.out.println("Web Server Application....");
+    }
+}
